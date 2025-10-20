dba17436b2b36b9994f74b8c9a52453a7c2ca080
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2025-10-20 10:12:24 +0700

    membuat app.post

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ db.connect((err) => {
     console.log('Connection Sucsessfully!');
 });
 
+//GET
 app.get('/api/users', (req, res) => {
     db.query('SELECT * FROM mahasiswa', (err, results) => {
         if (err) {
@@ -38,7 +39,29 @@ app.get('/api/users', (req, res) => {
         }
         res.json(results);
     })
-})
+});
+
+//POST
+app.post('/api/users', (req, res) => {
+    const { nama, nim, kelas } = req.body;
+
+    if (!nama || !nim || !kelas) {
+        return res.status(400).json({message: 'nama, nim, kelas wajib diisi'});
+    }
+
+    db.query(
+        'INSERT INTO mahasiswa (nama, nim, kelas) VALUES (?, ?, ?)',
+        [nama, nim, kelas],
+        (err, results) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({message: 'Database Error'});
+            }
+            res.status(201).json({message: 'User created successfully'});
+        }
+    );
+});
+    
 
 
 
